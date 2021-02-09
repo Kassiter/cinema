4154119995d861a31659a37408dbb12893896b32
Oleh Volosianskyi <o.volosianskyi@sloboda-studio.com>
2021-02-09 15:39:37 +0200

    Routes naming

--- api/urls.py
+++ api/urls.py
@@ -8,18 +8,18 @@ urlpatterns = [
   path("", views.apiOverview, name="api-overview"),
 
   path("movies/", MoviesAPIView.index, name="movies"),
-  path("movie/<str:id>/", MoviesAPIView.show, name="movie-detail"),
-  path("create-movie/", MoviesAPIView.create, name="create-movie"),
-  path("update-movie/<str:id>", MoviesAPIView.update, name="update-movie"),
-  path("delete-movie/<str:id>", MoviesAPIView.destroy, name="delete-movie"),
+  path("movies/<str:id>/", MoviesAPIView.show, name="movie-detail"),
+  path("movies/new/", MoviesAPIView.create, name="create-movie"),
+  path("movies/<str:id>/edit/", MoviesAPIView.update, name="update-movie"),
+  path("movies/<str:id>/delete/", MoviesAPIView.destroy, name="delete-movie"),
 
   path("genres/", GenresAPIView.index, name="genres"),
-  path("genre/<str:id>/", GenresAPIView.show, name="genre-detail"),
-  path("create-genre/", GenresAPIView.create, name="create-genre"),
-  path("update-genre/<str:id>", GenresAPIView.update, name="update-genre"),
-  path("delete-genre/<str:id>", GenresAPIView.destroy, name="delete-genre"),
+  path("genres/<str:id>/", GenresAPIView.show, name="genre-detail"),
+  path("genres/new/", GenresAPIView.create, name="create-genre"),
+  path("genres/<str:id>/edit/", GenresAPIView.update, name="update-genre"),
+  path("genres/<str:id>/delete/", GenresAPIView.destroy, name="delete-genre"),
 
-  path("user/<str:id>/", UsersAPIView.show, name="user-detail"),
+  path("users/<str:id>/", UsersAPIView.show, name="user-detail"),
 
   re_path(r'^registration/?$', RegistrationAPIView.as_view(), name='user_registration'),
   re_path(r'^login/?$', LoginAPIView.as_view(), name='user_login'),
